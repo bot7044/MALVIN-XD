15fd0cdc2154feca9e2f4d023a786168b4898642
bot7044 <bot821358@gmail.com>
2025-07-28 10:53:02 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: "malvin~RmdASYzR#pQdTaxAh32Iu0KX_CRKH9TL1A0fZFHmR8Sv1hEdTNuc",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "916290228620", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
